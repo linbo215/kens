8868fc74b9752af4d08feb5c161475418340c309
Katherine T Craig Katherine <linyubo215@outlook.com>
2026-06-23 08:05:07 +0800

    Update hb_telecom_scan.py

--- py/hb_telecom_scan.py
+++ py/hb_telecom_scan.py
@@ -14,8 +14,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 # --- 核心配置：支持不同网段对应不同端口 ---
 TARGET_CONFIG = {
     "139.214": 9901,
-    "222.169": 9901,    
-    "58.245": 8888
+    "222.169": 9901   
 }
 CHECK_PATH = "/iptv/live/1000.json?key=txipt"
 M3U_FILE = "py/hb_telecom.m3u"
